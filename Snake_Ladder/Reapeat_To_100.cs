8c71b230bd5f0c1ee32b08fb0fff59f2c92c0366
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 21:39:56 +0530

    [Tejaswini Bute] Add Reapeat to 100

---
+++ Snake_Ladder/Reapeat_To_100.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Snake_Ladder
+{
+    class Snake_Ladder
+{
+    public static void Main(string[] args)
+    {
+        int store = 0;
+
+        int positions = 0;
+        int check, dice;
+        int temp;
+
+
+        Console.WriteLine("REPEAT TILL WINNER REACHES WINNING POSITION :");
+    start:
+        for (int i = 0; i <= 100; i++)
+        {
+        rep:
+            Throw myObj1 = new Throw();
+            myObj1.Roll1();
+            dice = myObj1.Rollingdie;
+
+
+            Throw myObj2 = new Throw();
+            myObj2.Roll1();
+
+            check = myObj2.options;
+
+
+            store = +dice;
+            if (check == 0)//same
+            {
+                goto rep;
+            }
+
+            else //behind
+            {
+                temp = 100 - dice;
+
+            }
+            positions += temp;
+        }
+
+        if (positions < 0)
+        {
+            goto start;
+        }
+        Console.WriteLine(positions);
+    }
+
+}
+    }
+
+
