d23c6d8fbd9f3743b41a16554525bc859a0daa21
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 21:09:37 +0530

    [Tejaswini Bute] Add dice roll

---
+++ Snake_Ladder/DiceRoll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SnakeLadder
+{
+    public class Dice
+    {
+        public int Rollingdie;
+        public void roll()
+        {
+            Console.WriteLine("ROLL THE DICE BETWEEN 1 TO 6: " + Rollingdie);
+        }
+
+    }
+    class UC2
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Dice dice = new Dice();
+            dice.Rollingdie = random.Next(1, 7);
+            dice.roll();
+        }
+    }
+}
+
---
+++ Snake_Ladder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SnakeLadder
+{
+    class UC1
+    {
+        static void Main(string[] args)
+        {
+            const int starting_point = 0;
+            Console.WriteLine("SINGLE PLAYER AT START POSITION: " + starting_point);
+
+        }
+    }
+}
